f32214d709fa0a485429410777623951e08ad39e
simon <simon@keppgrund5.de>
2020-10-22 15:00:17 +0200

    test

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -1,5 +1,15 @@
 package controllers
+import akka.util.ByteString
+import io.jsonwebtoken.Jwts.header
+import play.api.http.HttpEntity
+import play.api.mvc.{ResponseHeader, Result}
 
 class GameController {
-
+/*
+  def index = Action {
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Strict(ByteString("Hello world!"), Some("text/plain"))
+    )
+  }*/
 }
